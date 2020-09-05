41ee07d3557b7d39434058c7bc87f8b1f55a629a
Kopynets Artem <bachik85@gmail.com>
2020-09-05 08:40:49 +0300

    добавил components

--- js/CardsAPI.js
+++ js/CardsAPI.js
@@ -1,40 +1,40 @@
-// export const API = 'http://cards.danit.com.ua/cards';
-// const token = sessionStorage.getItem('token');
-//
-// export function  getLogin(email, password) {
-//     return fetch( {
-//         url: `${API}/login`,
-//         method: 'POST',
-//         body: JSON.stringify({
-//             email: email,
-//             password: password
-//         })
-//     })
-// }
-//
-//
-//
-// export function  createCard(card) {
-//     return fetch({
-//         url: `${API}/cards`,
-//         method: 'POST',
-//         headers: {
-//             Authorization: `Bearer ${token}`
-//         },
-//         body: JSON.stringify(card)
-//     })
-// }
-//
-//
-// export  function  getCards() {
-//     return fetch({
-//         url: `${API}/cards`,
-//         method: 'POST',
-//         headers: {
-//             Authorization: `Bearer ${token}`
-//         }
-//     })
-// }
-//
+export const API = 'http://cards.danit.com.ua/cards';
+const token = sessionStorage.getItem('token');
+
+export function  getLogin(email, password) {
+    return fetch( {
+        url: `${API}/login`,
+        method: 'POST',
+        body: JSON.stringify({
+            email: email,
+            password: password
+        })
+    })
+}
+
+
+
+export function  createCard(card) {
+    return fetch({
+        url: `${API}/cards`,
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify(card)
+    })
+}
+
+
+export  function  getCards() {
+    return fetch({
+        url: `${API}/cards`,
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
+}
+
 
 
